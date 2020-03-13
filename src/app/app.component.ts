5527e4dbeab157abfe9bf22f7be90cbb97964b1c
Iskandar Umidov <hx2860bn@go.minnstate.edu>
2020-03-13 03:57:50 -0500

    product list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'pm-root',
+  template: `<div><h1>{{pageTitle}}</h1><pm-products></pm-products></div>`
 })
 export class AppComponent {
-  title = 'deborah';
+  pageTitle: string = 'My WebApp';
 }
